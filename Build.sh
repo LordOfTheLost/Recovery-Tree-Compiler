9576cc014d77c02772fa7e23593dfceb5c67a3fd
LordOfTheLost <48102549+LordOfTheLost@users.noreply.github.com>
2021-07-18 14:58:06 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -16,7 +16,7 @@ FOXINSTALLER="$FOXRECOVERY/installer/META-INF/com/google/android/update-binary"
 
 Patch_OFRP_Settings() {
 if [ ! -f $FOXRECOVERY/ADVANCEDXML ]; then sed -i "336,372 d" $FOXADVANCEDXML; touch $FOXRECOVERY/ADVANCEDXML; fi
-sed -i "s/<placement x=\"%col1_x_caption%\" y=\"%row5_2_y%\"\/>/<placement x=\"%col1_x_caption%\" y=\"%row4_1a_y%\"\/>/g" $FOXADVANCEDXML
+sed -i "s/<placement x=\"%col1_x_caption%\" y=\"%row3_1a_y%\"\/>/<placement x=\"%col1_x_caption%\" y=\"%row4_1a_y%\"\/>/g" $FOXADVANCEDXML
 sed -i "s/<placement x=\"0\" y=\"%row5_3_y%\" w=\"%screen_w%\" h=\"%bl_h4%\"\/>/<placement x=\"0\" y=\"%row4_2a_y%\" w=\"%screen_w%\" h=\"%bl_h4%\"\/>/g" $FOXADVANCEDXML
 # sed -i "s/Roboto/GoogleSans/g" $FOXFONTXML; # sed -i "s/value=\"n\"/value=\"s\"/g" $FOXFONTXML
 sed -i "s/<condition var1=\"of_hide_app_hint\" op=\"!=\" var2=\"1\"\/>/<condition var1=\"of_hide_app_hint\" op=\"!=\" var2=\"0\"\/>/g" $FOXADVANCEDXML
@@ -53,7 +53,11 @@ export OF_RUN_POST_FORMAT_PROCESS=1
 
 # BACKUP
 export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=0
-export OF_QUICK_BACKUP_LIST="/boot;/data;/system;/vendor;"
+case $DEVICE in
+beryllium) export OF_QUICK_BACKUP_LIST="/boot;/data;/system_root;/vendor;";;
+dipper) export OF_QUICK_BACKUP_LIST="/boot;/data;/system_root;/vendor;";;
+*) export OF_QUICK_BACKUP_LIST="/boot;/data;/system;/vendor;";;
+esac
 
 # Files
 export FOX_USE_GREP_BINARY=0
