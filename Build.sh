3dac45a4a198853e762e381677e6f30d32d65818
LordOfTheLost <48102549+LordOfTheLost@users.noreply.github.com>
2021-09-16 10:48:39 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -155,9 +155,9 @@ cp -f $COMPILER/unrootmagisk.zip $SHRPFILES/unrootmagisk.zip
 Build() {
 case $DEVICE in
 castor) VOFRP="$BUILD_DATE-(1)"; VSHRP="$BUILD_DATE-(1)"; ARCH="arm";;
-beryllium) VOFRP="$BUILD_DATE-(13)"; VSHRP="$BUILD_DATE-(1)"; ARCH="arm64";;
-dipper) VOFRP="$BUILD_DATE-(21)"; VSHRP="$BUILD_DATE-(1)"; ARCH="arm64";;
-vince) VOFRP="$BUILD_DATE-(6)"; VSHRP="$BUILD_DATE-(1)"; ARCH="arm64";;
+beryllium) VOFRP="$BUILD_DATE-(14)"; VSHRP="$BUILD_DATE-(1)"; ARCH="arm64";;
+dipper) VOFRP="$BUILD_DATE-(22)"; VSHRP="$BUILD_DATE-(1)"; ARCH="arm64";;
+vince) VOFRP="$BUILD_DATE-(7)"; VSHRP="$BUILD_DATE-(1)"; ARCH="arm64";;
 *) echo Please Write Device Name; exit 0;;
 esac
 case $RECOVERY_TYPE in
